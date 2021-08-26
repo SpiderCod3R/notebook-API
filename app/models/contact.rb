c14888996aba78367c8b4a56c0ff755b56f36f95
THUNDER MASTER <thndrmaster@gmail.com>
2021-08-26 09:46:41 -0300

    CRUD com Nested Atributes

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,32 +1,32 @@
 class Contact < ApplicationRecord
   belongs_to :kind, optional: true
   has_many :phones
+  accepts_nested_attributes_for :phones, allow_destroy: true
 
   def author
     "3D Academy"
   end
   
   def kind_description
-    self.kind.description
+    self.kind.description if self.kind.present?
   end
 
   def birthdate_br
     I18n.l(self.birthdate) unless self.birthdate.blank?
   end
-  
 
   def to_br
     {
       name: self.name,
       email: self.email,
-      kind: (self.kind.description if self.kind.present?),
-      birthdate: (I18n.l(self.birthdate) unless self.birthdate.blank?),
+      kind: kind_description,
+      birthdate: birthdate_br,
     }
   end
   
   def as_json(options={})
     h = super(options)
-    h[:birthdate]=(I18n.l(self.birthdate) unless self.birthdate.blank?)
+    h[:birthdate]=birthdate_br
     h
   end
 
@@ -34,7 +34,7 @@ class Contact < ApplicationRecord
   #   super(
   #     root: true,
   #     methods: [:birthdate_br, :author, :kind_description],
-  #     include: { kind: {only: :description}, phones: { only: {:}} },
+  #     include: { kind: {only: :description}, phones: { only: {:number} } },
   #   )
   # end
 end

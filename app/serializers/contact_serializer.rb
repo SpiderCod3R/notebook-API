7b83b2c77f38796a1651856a3df5d957da6ae62a
THUNDER MASTER <thndrmaster@gmail.com>
2021-08-27 14:05:24 -0300

    Ajustando default_locale URL

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,12 +1,18 @@
 class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate
-  belongs_to :kind
+
+  belongs_to :kind do
+    link(:kind) { kind_url(object.kind) }
+  end
+
   has_many :phones
   has_one :address
 
   meta do 
     { "made-on": "UDEMY JACKSON PIRES - Desenvolvendo REST / RESTful APIs com Ruby on Rails" }
   end
+
+  link(:self) { contact_url(object) }
 
   def attributes(*args)
     h = super(*args)

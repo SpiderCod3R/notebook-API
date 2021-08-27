14a57c160dbf0e434d2c4a552ba846b6980a7952
THUNDER MASTER <thndrmaster@gmail.com>
2021-08-27 14:57:27 -0300

    Atualizando request header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  before_action :ensure_json_request
+
+  # Não Aceita a requisição sem o HEADER FIELD na request
+  def ensure_json_request
+    return if request.headers["Accept"] =~ /vnd\.api\+json/
+    render nothing: true, status: 406
+  end
+  
 end

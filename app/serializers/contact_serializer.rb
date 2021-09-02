9187e00d3675a5affecb8bb24eaa9371dad91380
THUNDER MASTER <thndrmaster@gmail.com>
2021-09-02 11:33:15 -0300

    AULA 47 - CRUD com AMS com has_many (Phones)

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -17,13 +17,9 @@ class ContactSerializer < ActiveModel::Serializer
     { "made-on": "UDEMY JACKSON PIRES - Desenvolvendo REST / RESTful APIs com Ruby on Rails" }
   end
 
-  # link(:self) { contact_url(object) }
-
   def attributes(*args)
     h = super(*args)
-    # Retorna no Formato pt-BR
-    # h[:birthdate]=object.birthdate_br
-    h[:birthdate]=object.birthdate.to_time.iso8601
+    h[:birthdate]=object.birthdate.to_time.iso8601 unless object.birthdate.nil?
     h
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,12 @@ Rails.application.routes.draw do
     resource :kind, only: [:show]
     resource :kind, only: [:show], path: 'relationships/kind'
 
+    resource :phone, only: [:create, :update, :destroy]
+    resource :phone, only: [:create, :update, :destroy], path: 'relationships/phones'
+    
     resource :phones, only: [:show]
     resource :phones, only: [:show], path: 'relationships/phones'
-
+    
     resource :address, only: [:show, :update, :create, :destroy]
     resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/address'
   end

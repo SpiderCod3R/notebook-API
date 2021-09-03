42fd553c45dd48abc129776e703373db400f99e7
THUNDER MASTER <thndrmaster@gmail.com>
2021-09-03 10:48:59 -0300

    AULA 60 Versionamento de API com Query Params - contraints routes

---
+++ app/controllers/v1/contacts_controller.rb
@@ -0,0 +1,76 @@
+module V1
+  class ContactsController < ApplicationController
+    before_action :authenticate_user!
+    
+    # TOKEN = "15b552b25eaf4b741e501010006a108"
+    #include ActionController::HttpAuthentication::Token::ControllerMethods
+    
+    before_action :set_contact, only: [:show, :update, :destroy]
+
+    # GET /contacts
+    def index
+      @contacts = Contact.all
+
+      render json: @contacts
+    end
+
+    # GET /contacts/1
+    def show
+      render json: @contact, include: [:kind, :phones, :address]
+    end
+
+    # POST /contacts
+    def create
+      @contact = Contact.new(contact_params)
+
+      if @contact.save
+        render json: @contact, include: [:kind, :phones, :address], status: :created, location: @contact
+      else
+        render json: @contact.errors, status: :unprocessable_entity
+      end
+    end
+
+    # PATCH/PUT /contacts/1
+    def update
+      if @contact.update(contact_params)
+        render json: @contact, include: [:kind, :phones, :address]
+      else
+        render json: @contact.errors, status: :unprocessable_entity
+      end
+    end
+
+    # DELETE /contacts/1
+    def destroy
+      @contact.destroy
+    end
+
+    private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_contact
+        @contact = Contact.find(params[:id])
+      end
+
+      # Only allow a list of trusted parameters through.
+      def contact_params
+        # params.require(:contact).permit(
+        #   :name, :email, :birthdate, :kind_id,
+        #   phones_attributes: [:id, :number, :_destroy],
+        #   address_attributes: [:id, :street, :city]
+        # )
+        ActiveModelSerializers::Deserialization.jsonapi_parse(params)
+      end
+
+      # def authenticate
+      #   authenticate_or_request_with_http_token do |token, options|
+      #     secret_key = "34f83350b03742cd1"
+      #     _token_ = JWT.decode token, secret_key, true, { algorithm: 'HS256'}
+      #     # Compare the tokens is a time-constant manner, to mitigate
+      #     # Time Attack
+      #     # ActiveSupport::SecurityUtils.secure_compare(
+      #     #   ::Digest::SHA256.hexdigest(token),
+      #     #   ::Digest::SHA256.hexdigest(TOKEN)
+      #     # )
+      #   end
+      # end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,40 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
-  resources :auths, only: [:create]
+  # resources :auths, only: [:create]
+  
   resources :kinds
-  resources :contacts do
-    resource :kind, only: [:show]
-    resource :kind, only: [:show], path: 'relationships/kind'
+  
+  scope module: 'v1' do 
+    resources :contacts, constraints: lambda { |request| request.params[:version] == '1' } do
+      resource :kind, only: [:show]
+      resource :kind, only: [:show], path: 'relationships/kind'
 
-    resource :phone, only: [:create, :update, :destroy]
-    resource :phone, only: [:create, :update, :destroy], path: 'relationships/phones'
-    
-    resource :phones, only: [:show]
-    resource :phones, only: [:show], path: 'relationships/phones'
-    
-    resource :address, only: [:show, :update, :create, :destroy]
-    resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/address'
+      resource :phone, only: [:create, :update, :destroy]
+      resource :phone, only: [:create, :update, :destroy], path: 'relationships/phones'
+      
+      resource :phones, only: [:show]
+      resource :phones, only: [:show], path: 'relationships/phones'
+      
+      resource :address, only: [:show, :update, :create, :destroy]
+      resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/address'
+    end
   end
+
+  scope module: 'v2' do 
+    resources :contacts, constraints: lambda { |request| request.params[:version] == '2' } do
+      resource :kind, only: [:show]
+      resource :kind, only: [:show], path: 'relationships/kind'
+
+      resource :phone, only: [:create, :update, :destroy]
+      resource :phone, only: [:create, :update, :destroy], path: 'relationships/phones'
+      
+      resource :phones, only: [:show]
+      resource :phones, only: [:show], path: 'relationships/phones'
+      
+      resource :address, only: [:show, :update, :create, :destroy]
+      resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/address'
+    end
+  end
+  
   
 end
